bcf5d22691138241669f9caf805a519c7e6f77e3
horaterefe <horaterefe@gmail.com>
2022-07-06 07:52:05 -0700

    hot fix alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
+
 /**
- * print_alphabet - Print alphabet in lowercase using only `_putchar`
- * Return: 0
- */
+* print_alphabet -> prints the lowercase alphabets
+*/
+
 void print_alphabet(void)
 {
-	char c = 'a';
-
-	while (c <= 'z')
+	int j;
+	for (j= 'a'; j <= 'z'; j++)
 	{
-		_putchar(c);
-		c++;
+		_putchar(j);
 	}
 	_putchar('\n');
 }
