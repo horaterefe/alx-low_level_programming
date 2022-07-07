0a6641717a32916d28b9cd054dbaa4f69a790cf1
horaterefe <horaterefe@gmail.com>
2022-07-07 03:02:07 -0700

    The numbers speak for themselves edited

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9,
- * followed by a new line
+ * print_numbers - print the numbers from 0 to 9
+ * Description: You can only use _putchar twice
  */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	i = 0;
+	while (i < 10)
 	{
 		_putchar(i + '0');
+		i++;
 	}
-
 	_putchar('\n');
 }
