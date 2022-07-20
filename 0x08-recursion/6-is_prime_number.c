af2af67433886aa57cb0ca6140437404441789b9
horaterefe <horaterefe@gmail.com>
2022-07-20 08:58:48 -0700

    hot fix

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,26 +1,39 @@
 #include "main.h"
-
-int tmp_prime(int n, int i);
-
 /**
- * divisors - number is prime?
- * @n: integer params
- * @m: integer params
- * Return: boolean
+ * prime_found - found prime number
+ * @i: dividend
+ * @j: divider
+ *
+ * Return: return 1 if the input integer is prime number, otherwise 0
  */
-
-int divisors(int n, int m)
+int prime_found(int i, int j)
 {
-	if (m % n == 0)
+	if (j != 1)
 	{
-		return (0);
+		if (i == j)
+		{
+			return (1);
+		}
+		else if (j % i == 0 || j <= 1)
+		{
+			return (0);
+		}
+		else
+		{
+			return (prime_found(i + 1, j));
+		}
 	}
-	else if (m / 2 > n)
-	{
-		return (divisors(n + 2, m));
-	}
-	else
-	{
-		return (1);
-	}
+	return (0);
+}
+/**
+ * is_prime_number - shows if is a prime number
+ * @n: number
+ *
+ * Return: the input integer if its a prime number
+ */
+int is_prime_number(int n)
+{
+	int a = 2;
+
+	return (prime_found(a, n));
 }
