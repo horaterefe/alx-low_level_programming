ef18d611ca561020ce1bada6e2a8b7a02c0a3510
horaterefe <horaterefe@gmail.com>
2022-08-22 18:35:37 -0700

    fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,41 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * read_textfile - that reads a text file and prints
- * @filename: variable pointer
- * @letters: size letters
- * Description: Write a function that reads a text file and prints it
- * to the POSIX standard output.
- * Return: the actual number of letters it could read and print, 0 otherwise
+ * read_textfile - reads a text file and prints it to stdout
+ * @filename: file to be read
+ * @letters:  number of letters it should read and print
+ *
+ * Return: number of letters it could read and print
+ * if the file can not be opened or read, return 0
+ * if filename is NULL return 0
+ * if write fails or does not write the expected amount of bytes, return 0
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, let, w;
-	char *text;
+	size_t length = 0, final_length = 0;
+	int fd = 0;
+	char *buff = NULL;
 
-	text = malloc(letters);
-	if (text == NULL)
-		return (0);
 
 	if (filename == NULL)
-		return (0);
+		return (length);
 
-	file = open(filename, O_RDONLY);
+	fd = open(filename, O_RDWR | O_CREAT);
+	if (fd < 0)
+		return (length);
 
-	if (file == -1)
-	{
-		free(text);
-		return (0);
-	}
-
-	let = read(file, text, letters);
-
-	w = write(STDOUT_FILENO, text, let);
-
-	close(file);
-
-	return (w);
+	buff = malloc(sizeof(*buff) * letters);
+	if (buff == NULL)
+		return (length);
+	length = read(fd, buff, letters);
+	final_length = write(STDOUT_FILENO, buff, length);
+	if (final_length != length)
+		length = 0;
+	close(fd);
+	free(buff);
+	return (length);
 }
